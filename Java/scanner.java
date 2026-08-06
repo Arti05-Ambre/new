59cfae1181c1cf3cd240d362ff3e2601f55fd8d3
Arti05-Ambre <ambrearti62@gmail.com>
2026-08-06 22:58:47 +0530

    Update scanner.java

--- Java/scanner.java
+++ Java/scanner.java
@@ -12,7 +12,7 @@ package cv;
 						}
 			     		else if (num < 0) {
 			     			System.out.println("Number is negative");
-			     		} 
+			     		 } 
 			     		else {
 			     			System.out.println("Number is zero");
 			     
